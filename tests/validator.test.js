5c43a2a895190b6c24f9dbb5da7c139fc6a71904
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-10 15:57:23 -0700

    progress on Schema and Validator

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -2,18 +2,33 @@ const {
   Validator,
 } = require('../lib/validator.js');
 
-it('validates field', () => {
-  const fieldValidator = new Validator('field', {
-    type: String,
-    required: true
-  });
-  
-  expect(fieldValidator.validate({ field: 'spot' })).toEqual('spot');
+describe('validator module', () => {
+  describe('basic validation', () => {
+    it('has a field and configuration property', () => {
+      const colorValidator = new Validator('color', {
+        type: String,
+        required: true
+      });
+      
+      expect(colorValidator.field).toEqual('color');
+      expect (colorValidator.configuration).toEqual({
+        type: String,
+        required: true
+      })
+    });
 
-  const configurationValidator = new Validator('configuration', {
-    type: String,
-    required: true
-  });
-
-  expect(configurationValidator.validate({ configuration: 'spot' })).toEqual('spot');
+    it('can validate an object with the proper type', () => {
+      const colorValidator
+    })
 });
+
+
+// Test that validate method can take an object and return a fields value
+// Test that validate method can take an object and throw an error
+// Test all permutations:
+// required and field missing
+// required and field there but wrong type
+// required and field there and right type
+// not required and field missing
+// not required and field there but wrong type
+// not required and field there and right type
