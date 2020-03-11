745d8c88a5331fd3ddb87460566ac0618dd8eafb
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-10 17:44:06 -0700

    trying a new tack

--- tests/snapshots/schema.test.js
+++ tests/snapshots/schema.test.js
@@ -1,14 +1,31 @@
-const schema = require('./Schema');
+const Schema = require('./Schema.js');
 
 // Test that validate method returns the object with all fields cast
 describe('Schema validation', () => {
 
   it('properly returns the object with all fields cast', () => {
-    expect(schema.validate(ink)).toEqual({
+    const ink = {
       color: 'dark blue',
       owned: true,
       yards: 350
+    };
+    
+    const schema = new Schema ({
+      color: {
+        type: String,
+        required: true
+      },
+      owned: {
+        type: Boolean,
+        required: true
+      },
+      yards: {
+        type: Number
+      }
     });
+    const inkSchema = new Schema(schema);
+    
+    expect(inkSchema.validate(ink)).toEqual(ink);
   });
 
   it('throws an error if object doesn\'t follow the schema', () => {
